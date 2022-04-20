bb7fe72a10eaa9d781a2d1591b4ac5b4b3621f03
light0986 <82749910+light0986@users.noreply.github.com>
2022-04-20 17:43:40 +0800

    Add files via upload

---
+++ WPF_QRCode.cs
@@ -0,0 +1,42 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PedigreeManagement.Static
+{
+    public class WPF_QRCode
+    {
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public static Bitmap QRCode_Creator(string str)
+        {
+            QRCodeGenerator QR = new QRCodeGenerator();
+            QRCodeData cd = QR.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(cd);
+            Bitmap qrCodeImage = qRCode.GetGraphic(5);
+            return qrCodeImage;
+        }
+
+        public static ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                    handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+    }
+}
